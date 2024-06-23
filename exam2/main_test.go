cb293eabcb1a16360a141d69a4e1eb4825bb6af2
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-06-23 23:09:56 +0700

    add main test func decode

--- exam2/main_test.go
+++ exam2/main_test.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDecodeString(t *testing.T) {
+	tests := []struct {
+		encoded  string
+		expected string
+	}{
+		{
+			encoded:  "LLRR=",
+			expected: "210122",
+		},
+		{
+			encoded:  "==RLL",
+			expected: "000210",
+		},
+		{
+			encoded:  "=LLRR",
+			expected: "221012",
+		},
+		{
+			encoded:  "RRL=R",
+			expected: "012001",
+		},
+	}
+
+	for _, test := range tests {
+		resultString, _ := decodeString(test.encoded)
+		fmt.Printf("input = %s output = %s\n", test.encoded, resultString)
+		if resultString != test.expected {
+			t.Errorf("For encoded %s, expected %s but got %s", test.encoded, test.expected, resultString)
+		}
+	}
+}
