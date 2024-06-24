cc6a66cd80869f32adadb8db55cb17fcc5cf9e7b
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-06-24 21:57:21 +0700

    test againg

--- exam3/server/services/counting_server.go
+++ exam3/server/services/counting_server.go
@@ -3,7 +3,7 @@ package services
 import (
 	context "context"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"regexp"
 	"strings"
@@ -69,7 +69,7 @@ func fetchData(url string) (string, error) {
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", fmt.Errorf("failed to read response body: %v", err)
 	}
